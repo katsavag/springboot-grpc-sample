beed4bf24c9aa42fa76b28a3876e74c1b3fb7b95
evangeloskatsadouros <katsadouros.v@gmail.com>
2025-08-08 08:12:12 +0200

    Create a greeting example of gRPC service

---
+++ users-service/src/test/java/com/katsadourose/grpc_sample/users_service/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.katsadourose.grpc_sample.users_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
